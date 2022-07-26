73b283f9e9bdacdedc2f6617becd77513490244b
Csaba Kádár <sablablab@gmail.com>
2022-07-26 16:15:04 +0200

    [Pair programming] make SquareStatus not final

--- src/com/codecool/battleship/board/Square.java
+++ src/com/codecool/battleship/board/Square.java
@@ -3,10 +3,10 @@ package com.codecool.battleship.board;
 public class Square {
     private int x;
     private int y;
-    private final SquareStatus squareStatus;
+    private SquareStatus squareStatus;
 
 
-    public Square(int x, int y, SquareStatus squareStatus) {
+    public Square(int x, int y) {
         this.x = x;
         this.y = y;
         this.squareStatus = SquareStatus.EMPTY;
