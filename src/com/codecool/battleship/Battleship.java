8d8e708c3b0576853c94062fdf23723e73d3937a
Csaba Kádár <sablablab@gmail.com>
2022-07-26 16:12:15 +0200

    [Pair programming] add temp printed Board

--- src/com/codecool/battleship/Battleship.java
+++ src/com/codecool/battleship/Battleship.java
@@ -1,7 +1,11 @@
 package com.codecool.battleship;
 
+import com.codecool.battleship.board.Board;
+import com.codecool.battleship.board.Square;
 import com.codecool.battleship.utils.Display;
 import com.codecool.battleship.utils.Input;
+
+import java.util.Arrays;
 
 public class Battleship {
     public static void main(String[] args) {
@@ -12,6 +16,9 @@ public class Battleship {
         while (isRunning) {
             display.printMenu();
             input.chooseMenu();
+            Board board = new Board();
+            board.fillBoardWithSquares();
+            System.out.println(Arrays.toString(board.getBoard()));
             isRunning = false;
         }
     }
