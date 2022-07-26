f444a87bfd69010995fc76536fb032cd47f99b1b
Csaba Kádár <sablablab@gmail.com>
2022-07-26 14:52:21 +0200

    [Pair programming]add starting skeleton to Battleship

--- src/com/codecool/battleship/Battleship.java
+++ src/com/codecool/battleship/Battleship.java
@@ -1,4 +1,19 @@
 package com.codecool.battleship;
 
+import com.codecool.battleship.utils.Display;
+import com.codecool.battleship.utils.Input;
+
 public class Battleship {
+    public static void main(String[] args) {
+        Display display = new Display();
+        Input input = new Input();
+        
+        boolean isRunning = true;
+        while (isRunning) {
+            display.printMenu();
+            input.chooseMenu();
+            isRunning = false;
+        }
+    }
+
 }
