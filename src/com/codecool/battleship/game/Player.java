c76766d1b061514e35c7efe86a2d9be978a2614c
Csaba Kádár <sablablab@gmail.com>
2022-07-26 17:01:23 +0200

    [Pair programming]add fields and method to Player

--- src/com/codecool/battleship/game/Player.java
+++ src/com/codecool/battleship/game/Player.java
@@ -1,4 +1,24 @@
 package com.codecool.battleship.game;
+import com.codecool.battleship.ship.Ship;
+import java.util.List;
 
 public class Player {
+    int playerNumber;
+    private List<Ship> ships;
+
+    public Player(int playerNumber) {
+        this.playerNumber = playerNumber;
+    }
+
+    public boolean isAlive() {
+        // TODO iterate through all of the ship coordinates
+        return true;
+    }
+
+    public void addShipToList(Ship ship) {
+        ships.add(ship);
+    }
+    public List<Ship> getPlayerShipList(){
+        return this.ships;
+    }
 }
