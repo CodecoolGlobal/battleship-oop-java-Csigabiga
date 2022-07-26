89f0ad530e5d2974d50b244a7fe97886f980a158
Csaba Kádár <sablablab@gmail.com>
2022-07-26 14:46:47 +0200

    [Pair programming]add methods and fields to SquareStatus

--- src/com/codecool/battleship/board/Square.java
+++ src/com/codecool/battleship/board/Square.java
@@ -1,4 +1,18 @@
 package com.codecool.battleship.board;
 
 public class Square {
+    private int x;
+    private int y;
+    private final SquareStatus squareStatus;
+
+
+    public Square(int x, int y, SquareStatus squareStatus) {
+        this.x = x;
+        this.y = y;
+        this.squareStatus = SquareStatus.EMPTY;
+    }
+
+    public SquareStatus getSquareStatus() {
+        return squareStatus;
+    }
 }
