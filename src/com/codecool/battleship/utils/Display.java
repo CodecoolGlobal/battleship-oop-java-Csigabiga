a30ab77e04cae08a7dcc9ed4d4941523e16f8eeb
Csaba Kádár <sablablab@gmail.com>
2022-07-26 14:52:54 +0200

    [Pair programming]add printMenu method to Display

--- src/com/codecool/battleship/utils/Display.java
+++ src/com/codecool/battleship/utils/Display.java
@@ -1,4 +1,14 @@
 package com.codecool.battleship.utils;
 
 public class Display {
+    public void printMenu() {
+        System.out.println("""
+                Welcome to the BattleShip game!
+                Choose an option!
+                1. Start Game
+                2. Highscores
+                3. Exit Game""");
+
+    }
+
 }
