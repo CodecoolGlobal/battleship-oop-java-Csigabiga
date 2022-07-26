bc31721b9bd0045630b9ea2afc3de6af5ab2b5b0
Csaba Kádár <sablablab@gmail.com>
2022-07-26 16:13:08 +0200

    [Pair programming] add method to fill board

--- src/com/codecool/battleship/board/Board.java
+++ src/com/codecool/battleship/board/Board.java
@@ -5,6 +5,26 @@ public class Board {
 
     public Square[][] ocean;
 
+    public boolean isPlacementOk() {
+        return true;
+    }
 
-    public boolean isPlacementOk() {return true;}
+    public Board() {
+        this.ocean = new Square[10][10];
+
+    }
+    public void fillBoardWithSquares() {
+        for (int i= 0; i < 10; i++){
+            for (int j = 0; j < 10; j++) {
+                ocean[i][j] = new Square(i,j);
+            }
+        }
+    }
+    //todo DELETE ME AFTER TESTING
+    public Square[][] getBoard(){
+        return this.ocean;
+    }
+
+
 }
+
