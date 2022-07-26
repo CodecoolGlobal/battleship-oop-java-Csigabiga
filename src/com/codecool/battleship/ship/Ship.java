b409bd8ecdbc7c5e2df8df0074420aab4d8cc7b2
Csaba Kádár <sablablab@gmail.com>
2022-07-26 15:20:19 +0200

    [Pair programming]add skeleton to Ship

--- src/com/codecool/battleship/ship/Ship.java
+++ src/com/codecool/battleship/ship/Ship.java
@@ -1,4 +1,21 @@
 package com.codecool.battleship.ship;
 
 public class Ship {
+    private String shipType;
+    private List<Square> shipCoordinates;
+
+
+    public String getShipType() {
+        return shipType;
+    }
+
+
+    public void setShipType(String shipType) {
+        this.shipType = shipType;
+    }
+
+
+    public List<Square> getShipCoordinates() {
+        return shipCoordinates;
+    }
 }
