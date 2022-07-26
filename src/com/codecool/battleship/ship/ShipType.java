16c7f65f33c7e5ac63a829379241c786aa745b87
Csaba Kádár <sablablab@gmail.com>
2022-07-26 15:20:01 +0200

    [Pair programming]add skeleton to ShipType

---
+++ src/com/codecool/battleship/ship/ShipType.java
@@ -0,0 +1,20 @@
+package com.codecool.battleship.ship;
+
+public enum ShipType {
+    Carrier(5),
+    Battleship(4),
+    Cruiser(3),
+    Submarine(2),
+    Destroyer(1);
+
+
+    private final int shipLength;
+
+    ShipType(int length){
+        shipLength = length;
+    }
+
+    public int getShipLength() {
+        return shipLength;
+    }
+}
