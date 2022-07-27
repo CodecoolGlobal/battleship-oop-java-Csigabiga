471329e13a0c5e93f4aeb0759a7128610b389c55
Qephetzial <prekopadominik@gmail.com>
2022-07-27 16:16:05 +0200

    request input methods

--- src/com/codecool/battleship/utils/Input.java
+++ src/com/codecool/battleship/utils/Input.java
@@ -1,4 +1,15 @@
 package com.codecool.battleship.utils;
 
+import java.util.ArrayList;
+
 public class Input {
+
+
+    public String coordinates() {return "b2";}
+
+
+    public String direction() {return "up";}
+
+
+    public ArrayList<Integer> decoder(String coords) {return null;}
 }
