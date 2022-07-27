6f9bdb864ba60d6031f0f0dbfab0cb0d55f38a56
Qephetzial <prekopadominik@gmail.com>
2022-07-27 16:22:32 +0200

    add board

--- src/com/codecool/battleship/board/BoardFactory.java
+++ src/com/codecool/battleship/board/BoardFactory.java
@@ -2,10 +2,32 @@ package com.codecool.battleship.board;
 
 public class BoardFactory {
 
+    private final Board board;
 
+    public BoardFactory() {
+        this.board = new Board();
+        board.fillBoardWithSquares();
+    }
 
     public void randomPlacement() {}
 
 
     public void manualPlacement() {}
+}
+
+
+
+    public Board board() {
+        return board;
+    }
+
+
+    public Board placement(String type, ShipType ship) {
+        if (type.equals("manual")) {
+            manualPlacement(ship);
+        } else {
+            randomPlacement();
+        }
+        return board;
+    }
 }
