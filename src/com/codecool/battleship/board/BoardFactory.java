eda67ba37c308186cdab5ac59e9fe59b7769741f
Qephetzial <prekopadominik@gmail.com>
2022-07-27 16:22:57 +0200

    implement manual placement

--- src/com/codecool/battleship/board/BoardFactory.java
+++ src/com/codecool/battleship/board/BoardFactory.java
@@ -1,5 +1,11 @@
 package com.codecool.battleship.board;
 
+import com.codecool.battleship.ship.ShipType;
+import com.codecool.battleship.utils.Input;
+import com.codecool.battleship.utils.Validator;
+
+import java.util.ArrayList;
+
 public class BoardFactory {
 
     private final Board board;
@@ -9,11 +15,38 @@ public class BoardFactory {
         board.fillBoardWithSquares();
     }
 
-    public void randomPlacement() {}
+    private void randomPlacement() {}
 
 
-    public void manualPlacement() {}
-}
+    private boolean manualPlacement(ShipType ship) {
+        ArrayList<Integer> coordinates = Input.decoder(Input.coordinates());
+        int x = coordinates.get(0);
+        int y = coordinates.get(1);
+        boolean empty = board.isPlacementOk(x, y);
+        if (!empty) {
+            return false;
+        } else {
+            if (ship == ShipType.Destroyer) {
+                board.changeStatus(x, y, SquareStatus.SHIP);
+            } else {
+                String direction = Input.direction();
+                if (!Validator.validatePlacement(direction, ship, x, y, board)) {
+                    return false;
+                }
+                for (int i = 0; i < ship.getShipLength() + 1; i++) {
+                    int modifier = 0;
+                    switch (direction) {
+                        case "left" -> board.changeStatus(x - modifier, y, SquareStatus.SHIP);
+                        case "right" -> board.changeStatus(x + modifier, y, SquareStatus.SHIP);
+                        case "up" -> board.changeStatus(x, y + modifier, SquareStatus.SHIP);
+                        case "down" -> board.changeStatus(x, y - modifier, SquareStatus.SHIP);
+                    }
+                    modifier++;
+                }
+            }
+            return true;
+        }
+    }
 
 
 
