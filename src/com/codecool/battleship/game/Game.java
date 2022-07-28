0489e3d5b6f249f7a48c2cd45162aab8bff8285a
Qephetzial <prekopadominik@gmail.com>
2022-07-28 18:07:52 +0200

    commits

--- src/com/codecool/battleship/game/Game.java
+++ src/com/codecool/battleship/game/Game.java
@@ -14,21 +14,43 @@ public class Game {
 
     private final Display display;
 
-    public Game(Input input, Display display){
+    public Game(Input input, Display display) {
         this.input = input;
         this.display = display;
 
 
     }
 
-    public void gameLoop(int boardSize){
+
+    public void gameLoopAi(int boardSize) {
+        Player playerOne = createPlayer(boardSize);
+        Player playerTwo = createAi(boardSize);
+        boolean gameIsWon = false;
+        while(!gameIsWon){
+            playerOne.manualShoot(playerTwo.getPlayerBoardFactory().getBoard());
+            display.displayTwoBoard(playerOne.getPlayerBoardFactory().getBoard(), playerTwo.getPlayerBoardFactory().getBoard());
+            if(!playerTwo.isAlive()){
+                gameIsWon = true;
+                display.winMessage(playerOne.getPlayerName());
+            }
+            playerTwo.randomShoot(playerOne.getPlayerBoardFactory().getBoard());
+            if(!playerOne.isAlive()){
+                gameIsWon = true;
+                display.winMessage(playerTwo.getPlayerName());
+            }
+
+        }
+    }
+
+
+    public void gameLoop(int boardSize) {
         Player playerOne = createPlayer(boardSize);
         Player playerTwo = createPlayer(boardSize);
         boolean gameIsWon = false;
         Player currentPlayer = playerOne;
         Player enemyPlayer = playerTwo;
         while(!gameIsWon){
-            currentPlayer.shoot(enemyPlayer.getPlayerBoardFactory().getBoard());
+            currentPlayer.manualShoot(enemyPlayer.getPlayerBoardFactory().getBoard());
             display.displayTwoBoard(currentPlayer.getPlayerBoardFactory().getBoard(), enemyPlayer.getPlayerBoardFactory().getBoard());
             if(!enemyPlayer.isAlive()){
                 gameIsWon = true;
@@ -56,6 +78,17 @@ public class Game {
             playerClass.addShipToList(playerClass.getPlayerBoardFactory().placement(placementAnswer, ShipType.values()[i]));
         }
         display.displayOneBoard(playerClass.getPlayerBoardFactory().getBoard());
+
+        return playerClass;
+    }
+
+
+    private Player createAi(int boardSize) {
+        Player playerClass = new Player("Ai", boardSize);
+        for (int i = 0; i < ShipType.values().length;i++){
+            display.displayOneBoard(playerClass.getPlayerBoardFactory().getBoard());
+            playerClass.addShipToList(playerClass.getPlayerBoardFactory().placement(2, ShipType.values()[i]));
+        }
 
         return playerClass;
     }
