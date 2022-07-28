5f0c6925182b0de8a90f0387b82d327e8fdfac56
Csaba Kádár <sablablab@gmail.com>
2022-07-28 14:14:59 +0200

    [PAIR PROGRAMMING] changes made by Dominik

--- src/com/codecool/battleship/board/BoardFactory.java
+++ src/com/codecool/battleship/board/BoardFactory.java
@@ -23,7 +23,7 @@ public class BoardFactory {
         directions.add("down");
         directions.add("right");
         directions.add("left");
-        int index = ThreadLocalRandom.current().nextInt(0, 2 + 1);
+        int index = ThreadLocalRandom.current().nextInt(0, directions.size());
         boolean empty = false;
         while (!empty) {
             int x = ThreadLocalRandom.current().nextInt(0, this.board.getBoardSize() + 1);
@@ -40,10 +40,10 @@ public class BoardFactory {
                         int modifier = 0;
                         for (int i = 0; i < ship.getShipLength(); i++) {
                             switch (direction) {
-                                case "left" -> board.changeStatus(x - modifier, y, SquareStatus.SHIP);
-                                case "right" -> board.changeStatus(x + modifier, y, SquareStatus.SHIP);
-                                case "down" -> board.changeStatus(x, y + modifier, SquareStatus.SHIP);
-                                case "up" -> board.changeStatus(x, y - modifier, SquareStatus.SHIP);
+                                case "up" -> board.changeStatus(x - modifier, y, SquareStatus.SHIP);
+                                case "down" -> board.changeStatus(x + modifier, y, SquareStatus.SHIP);
+                                case "right" -> board.changeStatus(x, y + modifier, SquareStatus.SHIP);
+                                case "left" -> board.changeStatus(x, y - modifier, SquareStatus.SHIP);
                             }
                             modifier++;
                         }
@@ -66,17 +66,17 @@ public class BoardFactory {
         if (ship == ShipType.Destroyer) {
             board.changeStatus(x, y, SquareStatus.SHIP);
         } else {
-            String direction = "down"; //Input.direction();
+            String direction = Input.direction();
             if (!validatePlacement(direction, ship, x, y)) {
                 return false;
             }
             int modifier = 0;
             for (int i = 0; i < ship.getShipLength(); i++) {
                 switch (direction) {
-                    case "left" -> board.changeStatus(x - modifier, y, SquareStatus.SHIP);
-                    case "right" -> board.changeStatus(x + modifier, y, SquareStatus.SHIP);
-                    case "down" -> board.changeStatus(x, y + modifier, SquareStatus.SHIP);
-                    case "up" -> board.changeStatus(x, y - modifier, SquareStatus.SHIP);
+                    case "up" -> board.changeStatus(x - modifier, y, SquareStatus.SHIP);
+                    case "down" -> board.changeStatus(x + modifier, y, SquareStatus.SHIP);
+                    case "right" -> board.changeStatus(x, y + modifier, SquareStatus.SHIP);
+                    case "left" -> board.changeStatus(x, y - modifier, SquareStatus.SHIP);
                 }
                 modifier++;
             }
@@ -103,28 +103,28 @@ public class BoardFactory {
 
     private boolean validatePlacement(String direction, ShipType ship, int x, int y) {
         switch (direction) {
-            case "left":
+            case "up":
                 for (int i = 0; i < ship.getShipLength(); i++) {
                     if (x - i < 0) {return false;}
                     boolean empty = board.isPlacementOk(x - i, y);
                     if (!empty) {return false;}
                 }
                 break;
-            case "right":
+            case "down":
                 for (int i = 0; i < ship.getShipLength(); i++) {
                     if (x + i > board.getBoardSize()) {return false;}
                     boolean empty = board.isPlacementOk(x + i, y);
                     if (!empty) {return false;}
                 }
                 break;
-            case "down":
+            case "right":
                 for (int i = 0; i < ship.getShipLength(); i++) {
-                    if (y + i > board.getBoardSize()) {return false;}
+                    if (y + i > board.getBoardSize()-1) {return false;}
                     boolean empty = board.isPlacementOk(x, y + i);
                     if (!empty) {return false;}
                 }
                 break;
-            case "up":
+            case "left":
                 for (int i = 0; i < ship.getShipLength(); i++) {
                     if (y - i < 0) {return false;}
                     boolean empty = board.isPlacementOk(x, y - i);
