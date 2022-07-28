42df6ae0472eb83a39b64b2efa7bb9c1ee654d25
Csaba Kádár <sablablab@gmail.com>
2022-07-28 11:07:55 +0200

    add better skeleton to Battleship

--- src/com/codecool/battleship/Battleship.java
+++ src/com/codecool/battleship/Battleship.java
@@ -1,18 +1,19 @@
-package com.codecool.battleship;
+package  com.codecool.battleship;
 
 import com.codecool.battleship.board.Board;
 import com.codecool.battleship.board.BoardFactory;
+import com.codecool.battleship.game.Game;
 import com.codecool.battleship.utils.Display;
 import com.codecool.battleship.utils.Input;
 import com.codecool.battleship.utils.MenuSelector;
 
-import java.util.Arrays;
 
 public class Battleship {
     public static void main(String[] args) {
         Input input = new Input();
         Display display = new Display(input);
         MenuSelector menu = new MenuSelector();
+        Game game = new Game(input, display);
 
         boolean isRunning = true;
         while (isRunning) {
@@ -20,15 +21,17 @@ public class Battleship {
             display.printMenu();
             int selectedMenuNumber = input.validateInput(3, 1);
             int selectedSubMenuNumber = menu.chooseMenu(selectedMenuNumber, display);
+            switch (selectedSubMenuNumber){
+                case 1:
+                    display.printDisplayField(Display.SpecifyBoardSizeMsg);
+                    int boardSize = input.validateInput(10, 5);
+                    game.gameLoop(boardSize);
 
-            BoardFactory board = new BoardFactory(10);
-            // TODO temp fix for running the Battleship.java
-            Board gameBoard = board.getBoard();
-            System.out.println(Arrays.toString(gameBoard.getBoard()));
-            display.displayOneBoard(gameBoard);
-            display.displayTwoBoard(gameBoard, gameBoard);
+                case 2:
+                    //TODO implement high score
+                    break;
+            }
 
-            isRunning = false;
         }
     }
 }
