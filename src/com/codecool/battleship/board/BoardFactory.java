8788c0e83383d0212e2c5a7a3e0c8d2c77e150aa
Qephetzial <prekopadominik@gmail.com>
2022-07-28 13:48:53 +0200

    refactor placement validation

--- src/com/codecool/battleship/board/BoardFactory.java
+++ src/com/codecool/battleship/board/BoardFactory.java
@@ -25,7 +25,7 @@ public class BoardFactory {
         directions.add("left");
         int index = ThreadLocalRandom.current().nextInt(0, 2 + 1);
         boolean empty = false;
-        while (empty) {
+        while (!empty) {
             int x = ThreadLocalRandom.current().nextInt(0, this.board.getBoardSize() + 1);
             int y = ThreadLocalRandom.current().nextInt(0, this.board.getBoardSize() + 1);
             empty = board.isPlacementOk(x, y);
@@ -42,8 +42,8 @@ public class BoardFactory {
                             switch (direction) {
                                 case "left" -> board.changeStatus(x - modifier, y, SquareStatus.SHIP);
                                 case "right" -> board.changeStatus(x + modifier, y, SquareStatus.SHIP);
-                                case "up" -> board.changeStatus(x, y + modifier, SquareStatus.SHIP);
-                                case "down" -> board.changeStatus(x, y - modifier, SquareStatus.SHIP);
+                                case "down" -> board.changeStatus(x, y + modifier, SquareStatus.SHIP);
+                                case "up" -> board.changeStatus(x, y - modifier, SquareStatus.SHIP);
                             }
                             modifier++;
                         }
@@ -74,8 +74,8 @@ public class BoardFactory {
                 switch (direction) {
                     case "left" -> board.changeStatus(x - modifier, y, SquareStatus.SHIP);
                     case "right" -> board.changeStatus(x + modifier, y, SquareStatus.SHIP);
-                    case "up" -> board.changeStatus(x, y + modifier, SquareStatus.SHIP);
-                    case "down" -> board.changeStatus(x, y - modifier, SquareStatus.SHIP);
+                    case "down" -> board.changeStatus(x, y + modifier, SquareStatus.SHIP);
+                    case "up" -> board.changeStatus(x, y - modifier, SquareStatus.SHIP);
                 }
                 modifier++;
             }
@@ -104,24 +104,28 @@ public class BoardFactory {
         switch (direction) {
             case "left":
                 for (int i = 0; i < ship.getShipLength(); i++) {
+                    if (x - i < 0) {return false;}
                     boolean empty = board.isPlacementOk(x - i, y);
                     if (!empty) {return false;}
                 }
                 break;
             case "right":
                 for (int i = 0; i < ship.getShipLength(); i++) {
+                    if (x + i > board.getBoardSize()) {return false;}
                     boolean empty = board.isPlacementOk(x + i, y);
-                    if (!empty) {return false;}
-                }
-                break;
-            case "up":
-                for (int i = 0; i < ship.getShipLength(); i++) {
-                    boolean empty = board.isPlacementOk(x, y + i);
                     if (!empty) {return false;}
                 }
                 break;
             case "down":
                 for (int i = 0; i < ship.getShipLength(); i++) {
+                    if (y + i > board.getBoardSize()) {return false;}
+                    boolean empty = board.isPlacementOk(x, y + i);
+                    if (!empty) {return false;}
+                }
+                break;
+            case "up":
+                for (int i = 0; i < ship.getShipLength(); i++) {
+                    if (y - i < 0) {return false;}
                     boolean empty = board.isPlacementOk(x, y - i);
                     if (!empty) {return false;}
                 }
