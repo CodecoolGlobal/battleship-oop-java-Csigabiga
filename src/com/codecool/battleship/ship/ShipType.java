ebcd1744c6f25415f258d79fd43f8dfb30f1692d
Qephetzial <prekopadominik@gmail.com>
2022-07-28 18:07:59 +0200

    commits

--- src/com/codecool/battleship/ship/ShipType.java
+++ src/com/codecool/battleship/ship/ShipType.java
@@ -1,9 +1,9 @@
 package com.codecool.battleship.ship;
 
 public enum ShipType {
-//    Carrier(5),
-//    Battleship(4),
-//    Cruiser(3),
+    Carrier(5),
+    Battleship(4),
+    Cruiser(3),
     Submarine(2),
     Destroyer(1);
 
