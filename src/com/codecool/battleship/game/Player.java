dd982d2ce7dad619dd8ba22a6996015ca7dfcec3
Qephetzial <prekopadominik@gmail.com>
2022-07-28 10:54:14 +0200

    add shoot function

--- src/com/codecool/battleship/game/Player.java
+++ src/com/codecool/battleship/game/Player.java
@@ -1,5 +1,9 @@
 package com.codecool.battleship.game;
 import com.codecool.battleship.ship.Ship;
+import com.codecool.battleship.utils.Decoder;
+import com.codecool.battleship.utils.Input;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
@@ -20,5 +24,25 @@ public class Player {
     }
     public List<Ship> getPlayerShipList(){
         return this.ships;
+    }
+
+
+    public boolean shoot(Board board) {
+        ArrayList<Integer> coordinates = Decoder.decoder(Input.coordinates());
+        int x = coordinates.get(0);
+        int y = coordinates.get(1);
+        SquareStatus state = board.squareStatus(x, y);
+        switch (state) {
+            case EMPTY:
+                board.changeStatus(x, y, SquareStatus.MISS);
+                return true;
+            case SHIP:
+                board.changeStatus(x, y, SquareStatus.HIT);
+                return true;
+            case HIT:
+            case MISS:
+                return false;
+        }
+        return false;
     }
 }
